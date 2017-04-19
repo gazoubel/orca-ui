a29970f51bda811864dfd698cf14887228ffef26
gazoubel <gazoubel@gmail.com>
2017-04-18 23:15:12 -0400

    improved moment

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
-import momentComputed from 'ember-moment/computeds/moment';
-import format from 'ember-moment/computeds/format';
-import locale from 'ember-moment/computeds/locale';
+import moment from 'moment';
+// import momentComputed from 'ember-moment/computeds/moment';
+// import format from 'ember-moment/computeds/format';
+// import locale from 'ember-moment/computeds/locale';
 
 export default Ember.Helper.extend({
   moment: Ember.inject.service(),
 
   compute: function(params) {
-    let value = params[0];
-    
+    // let value = params[0];
+
     return moment(params[0]).format(this.get('moment.defaultFormat'));
+    // return this.get('moment').format(params[0], this.get('moment.defaultFormat'));
+    // let mom = momentComputed(params[0]);
+    // let value = format(mom, this.get('moment.defaultFormat'));
+    // return value.toString();
   }
 });
