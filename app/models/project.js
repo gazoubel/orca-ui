560e42ce8d7ca60bcd300ef657429ae2faf752b4
gazoubel <gazoubel@gmail.com>
2017-11-15 00:40:19 -0500

    improved health track

--- app/models/project.js
+++ app/models/project.js
@@ -35,6 +35,24 @@ export default DS.Model.extend(Validations,{
       return (prev || 0) + Number(item.get('total'));
     });
   }),
+  totalPurchase: Ember.computed( 'projectStages.@each.totalPurchase', 'projectStages.[]',function() {
+    var projectStages = this.get('projectStages');
+    if (!projectStages) {
+      return 0;
+    }
+    return projectStages.reduce(function(prev, item) {
+      return (prev || 0) + Number(item.get('totalPurchase'));
+    });
+  }),
+  totalPayments: Ember.computed( 'projectStages.@each.totalPayments', 'projectStages.[]',function() {
+    var projectStages = this.get('projectStages');
+    if (!projectStages) {
+      return 0;
+    }
+    return projectStages.reduce(function(prev, item) {
+      return (prev || 0) + Number(item.get('totalPayments'));
+    });
+  }),
   predictedTotal: Ember.computed( 'projectStages.@each.predictedTotal', 'projectStages.[]',function() {
     var projectStages = this.get('projectStages');
     if (!projectStages) {
@@ -45,6 +63,19 @@ export default DS.Model.extend(Validations,{
     });
   }),
 
+  predictedTotalForNonWaitingStages: Ember.computed('projectStages.@each.isWaiting', 'projectStages.@each.predictedTotal', 'projectStages.[]',function() {
+    var projectStages = this.get('projectStages');
+    if (!projectStages) {
+      return 0;
+    }
+    return projectStages.reduce(function(prev, item) {
+      return (prev || 0) +  (item.get('isWaiting')===true? 0: Number(item.get('predictedTotal')));
+      // return item.get('isWaiting').then(function(isWaiting){
+      //   return (prev || 0) +  (isWaiting? 0: Number(item.get('predictedTotal')));
+      // })
+    });
+  }),
+
   finishedStages: Ember.computed( 'projectStages.@each.isFinished',function() {
     var projectStages = this.get('projectStages');
     if (!projectStages || projectStages.get('length')===0) {
@@ -52,6 +83,15 @@ export default DS.Model.extend(Validations,{
     }
     return projectStages.reduce(function(prev, item) {
       return (prev || 0) + (item.get('isFinished')?1:0);
+    });
+  }),
+  activeOrClosedStages: Ember.computed( 'projectStages.@each.isWaiting',function() {
+    var projectStages = this.get('projectStages');
+    if (!projectStages || projectStages.get('length')===0) {
+      return 0;
+    }
+    return projectStages.reduce(function(prev, item) {
+      return (prev || 0) + (item.get('isWaiting')?0:1);
     });
   }),
 
@@ -80,4 +120,18 @@ export default DS.Model.extend(Validations,{
 
   wasNotProfitable: Ember.computed.not('wasProfitable'),
 
+  percentageSpent: Ember.computed('totalSpent', 'predictedTotalForNonWaitingStages', function() {
+    var total = this.get('totalSpent')||0;
+    var predictedTotal = this.get('predictedTotalForNonWaitingStages') || 0;
+
+    if (predictedTotal===0) {
+      return 100;
+    }
+    return ( (total*100) / predictedTotal );
+  }),
+
+  isOnTrack: Ember.computed('percentageSpent', function() {
+    var percentageSpent = this.get('percentageSpent')||0;
+    return  percentageSpent<=100;
+  }),
 });
